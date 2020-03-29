cd0abbcce9b7405d077fbd36365d35636fab2dcc
waterval <luukvla@hotmail.com>
2020-03-29 14:10:16 +0200

    added styling

--- src/components/pokemon-page/energy.tsx
+++ src/components/pokemon-page/energy.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import Type from "../pokemon-page/type";
 import Wrapper from "../pokemon-page/wrapper";
+import * as styles from "../../templates/pokemon-page.module.css";
 
 const Energy = props => (
-    <Wrapper className={"energy"}>
+    <Wrapper className={styles.energy}>
         {props.energy.map((type, index) => {
             return <Type type={type} key={index} />;
         })}
