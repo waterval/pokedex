36482322e1d08b5f768614545e0f02280cd2e44b
waterval <luukvla@hotmail.com>
2020-03-29 14:29:37 +0200

    updated website icon and title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,58 @@
 require(`dotenv`).config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+    path: `.env.${process.env.NODE_ENV}`
+});
 
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter 2`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Pokémon - Pokédex: First Generation`,
+        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+        author: `@gatsbyjs`
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-source-graphql`,
-      options: {
-        // This type will contain remote schema Query type
-        typeName: `POKEMON`,
-        // This is the field under which it's accessible
-        fieldName: `pokeAPI`,
-        // URL to query from
-        url: `https://graphql-pokemon.now.sh/`,
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-    `gatsby-plugin-typescript`,
-  ],
-}
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`
+            }
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/pokedex-icon.png` // This path is relative to the root of the site.
+            }
+        },
+        {
+            resolve: `gatsby-source-graphql`,
+            options: {
+                // This type will contain remote schema Query type
+                typeName: `POKEMON`,
+                // This is the field under which it's accessible
+                fieldName: `pokeAPI`,
+                // URL to query from
+                url: `https://graphql-pokemon.now.sh/`
+            }
+        },
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: [`Molengo`, `Lekton`],
+                display: "swap"
+            }
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+        `gatsby-plugin-typescript`,
+        `gatsby-transformer-typescript-css-modules`
+    ]
+};
