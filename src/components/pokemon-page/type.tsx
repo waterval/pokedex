be220dccf4000d94b2e7898ef4a6af2293b471de
waterval <luukvla@hotmail.com>
2020-03-29 14:18:01 +0200

    added corresponding color of each energy type

--- src/components/pokemon-page/type.tsx
+++ src/components/pokemon-page/type.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
 import Wrapper from "../pokemon-page/wrapper";
+import * as styles from "../../templates/pokemon-page.module.css";
+import { typeToColor } from "../general/typeToColor";
 
 const Type = props => (
-    <Wrapper className={"type"} key={props.index}>
+    <Wrapper
+        className={styles.type}
+        key={props.index}
+        style={{
+            backgroundColor: typeToColor[props.type]
+        }}
+    >
         {props.type}
     </Wrapper>
 );
