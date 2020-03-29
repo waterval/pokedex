569466737564ab303c016452f30e5cf23dd223c0
waterval <luukvla@hotmail.com>
2020-03-29 15:28:37 +0200

    added pathprefix for deployment on github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
         description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
         author: `@gatsbyjs`
     },
+    pathPrefix: "/pokedex",
     plugins: [
         `gatsby-plugin-react-helmet`,
         {
