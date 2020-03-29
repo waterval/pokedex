6e3d6a54f356389f06d17688388ce57e60cf6303
waterval <luukvla@hotmail.com>
2020-03-29 14:17:28 +0200

    colorized the power of an attack

--- src/components/pokemon-page/progress.tsx
+++ src/components/pokemon-page/progress.tsx
@@ -1,7 +1,20 @@
 import * as React from "react";
+import { typeToColor } from "../general/typeToColor";
+import * as styles from "../../templates/pokemon-page.module.css";
 
-const Progress = props => (
-    <progress value={props.value} max={props.max}></progress>
-);
+const Progress = props => {
+    let proportionedWidth = (props.value / props.max) * 100 + "%";
+    return (
+        <div className={styles.progressContainer}>
+            <div
+                className={styles.progressItem}
+                style={{
+                    width: proportionedWidth,
+                    backgroundColor: typeToColor[props.type]
+                }}
+            ></div>
+        </div>
+    );
+};
 
 export default Progress;
